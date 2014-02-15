b88a25c6b7a922ddfddfdb7d3775716c69071275
Philip Woo <jhw1202@gmail.com>
2014-02-15 16:46:20 -0700

    super commit because best practices are for noobs: can select num of players, enter everyone's name, each takes turns to find out who they are then land on page to enter whether leader's group gets yay'd or nay'd to go on the mission; only naying a mission works right now.

--- js/game.js
+++ js/game.js
@@ -1,121 +1,167 @@
-window.App = {}
-App.Models = {}
-App.current = {}
 $(document).ready(function(){
-  $("#start-game").click(function(){
-    $('.landing').hide()
-    $(".num-of-players").show()
+  App.Models.Game = Backbone.Model.extend({
+    initialize: function(gameData){
+      this.numPlayers = gameData.numPlayers
+      this.numSpies = this.getNumSpies()
+      this.numResistance = this.numPlayers - this.numSpies
+      this.resistanceScore = 0
+      this.spyScore = 0
+      this.rejectionCount = 0
+      this.missionCount = 0
+    },
+
+    resetRejection: function() {
+      this.rejectionCount = 0
+    },
+
+    addRejection: function() {
+      this.rejectionCount += 1
+    },
+
+    getNumSpies: function(){
+      var numSpies
+      switch(this.numPlayers)
+      {
+      case 5:
+        numSpies = 2
+        break
+      case 6:
+        numSpies = 2
+        break
+      case 7:
+        numSpies = 3
+        break
+      case 8:
+        numSpies = 3
+        break
+      case 9:
+        numSpies = 3
+        break
+      case 10:
+        numSpies = 4
+        break
+      }
+      return numSpies
+    },
+
+    startRound: function(){
+      this.missionCount++
+      $(".mission-vote").show()
+      this.currentMission = new App.Models.Mission({missionNum: this.missionCount})
+      var currentLeader = App.current.players.currentLeader
+      debugger
+      $(".rejection-count").text(this.rejectionCount)
+      $(".mission-number").text(this.missionCount)
+      $(".mission-players-num").text(this.currentMission.numMembers)
+      $(".fails-required").text(this.currentMission.failsRequired)
+      $(".mission-leader").text(currentLeader.name)
+    },
+
+    continueRound: function() {
+      var currentLeader = App.current.players.incrementLeader()
+      this.rejectionCount++
+      $(".rejection-count").text(this.rejectionCount)
+      $(".mission-number").text(this.missionCount)
+      $(".mission-leader").text(currentLeader.name)
+    }
   })
 
-  $("#submit-num-players").click(function(){
-    $(".num-of-players").hide()
-    var numPlayers = parseInt($("#num-players option:selected").val())
-    App.current.game = new App.Models.Game({numPlayers: numPlayers})
-  })
-})
+  App.Models.Mission = Backbone.Model.extend({
+    initialize: function(missionData){
+      this.missionNum = missionData.missionNum
+      this.failsRequired = this.failsRequired()
+      this.numMembers = window.missionNumbers[App.current.game.numPlayers][this.missionNum]
+    },
 
-App.Models.Game = Backbone.Model.extend({
-  initialize: function(gameData){
-    this.numPlayers = gameData.numPlayers
-    this.numSpies = this.getNumSpies()
-    this.numResistance = this.numPlayers - this.numSpies
-    this.resistanceScore = 0
-    this.spyScore = 0
-    this.rejectionCount = 0
-  },
-
-  resetRejection: function() {
-    this.rejectionCount = 0
-  },
-
-  addRejection: function() {
-    this.rejectionCount += 1
-  },
-
-  getNumSpies: function(){
-    var numSpies
-    switch(this.numPlayers)
-    {
-    case 5:
-      numSpies = 2
-      break
-    case 6:
-      numSpies = 2
-      break
-    case 7:
-      numSpies = 3
-      break
-    case 8:
-      numSpies = 3
-      break
-    case 9:
-      numSpies = 3
-      break
-    case 10:
-      numSpies = 4
-      break
-    }
-    return numSpies
-  }
-})
-
-var Mission = Backbone.Model.extend({
-  initialize: function(missionData){
-    this.missionNum = missionData.missionNum
-    this.numPlayers = App.Models.Game.numPlayers
-    this.failsRequired = this.failsRequired()
-  },
-
-  failsRequired: function() {
-    var failsRequired
-    numPlayers = this.numPlayers
-    missionNum = this.missionNum
-    missionDataAsString = this.dataTostring()
-    if(this.numPlayers === 5 || this.numPlayers === 6) {
-      failsRequired = 1
-    }
-    else if(missionNum === 4) {
-      if(numPlayers >= 7) {
+    failsRequired: function() {
+      var failsRequired
+      if(App.current.game.numPlayers === (5 || 6)) {
+        failsRequired = 1
+      }
+      else if(missionNum === 4) {
         failsRequired = 2
       }
+      return failsRequired
     }
-    else {
-      failsRequired = 1
+  })
+
+  App.Models.Player = Backbone.Model.extend({
+    initialize: function(data) {
+      this.name = data.name
+      this.id = data.id
+      this.identity = "resistance"
+    },
+
+    displayIdentity: function() {
+      $(".find-out .name").text(this.name)
+      $("p.identity").text("You are a " + this.identity)
+      $(".find-out").show()
+      $(".reveal-identity").show()
+
+      $(".reveal-identity").click(function(){
+        $(this).hide()
+        $("p.identity").show()
+      })
     }
-    return failsRequired
-  }
+  })
+
+  App.Collections.Players = Backbone.Collection.extend({
+    model: App.Models.Player,
+
+    createSpies: function(numSpies) {
+      var players = _.shuffle(this.models)
+      var i = 0
+      _.times(numSpies, function(){
+        players[i].identity = "spy"
+        i++
+      })
+      this.displayOrder = 1
+    },
+
+    displayIdentities: function(){
+      if(this.displayOrder === this.models.length + 1) {
+        debugger
+        $(".find-out").hide()
+        App.current.game.startRound()
+        this.currentLeader = this.get(1)
+      }
+      else {
+        this.get(this.displayOrder).displayIdentity()
+        this.displayOrder++
+      }
+    },
+    incrementLeader: function(){
+      if(this.currentLeader.id === this.models.length){
+        this.currentLeader = this.get(1)
+      }
+      else {
+        this.currentLeader = this.get(this.currentLeader.id + 1)
+      }
+      return this.currentLeader
+    }
+  })
+
+
+
+  // Player:
+  //   -team
+
+  // Game:
+  //   -resistanceScore
+  //   -spyScore
+  //   -rejectionCount(in a row)
+  //   -currentLeader
+  //   -#of resistance
+  //   -# of spies
+
+
+  // Mission
+  //   -# of players
+  //   -status
+  //   -# of fails required
+
+  // Round Vote
+
+  // Mission Vote
+
 })
-
-var Round = Backbone.Model.extend({
-  initialize: function() {
-  },
-
-  approved: function() {
-    App.Models.Game.resetRejection()
-  },
-
-  denied: function() {
-    App.Models.Game.addRejection()
-  }
-})
-
-// Player:
-//   -team
-
-// Game:
-//   -resistanceScore
-//   -spyScore
-//   -rejectionCount(in a row)
-//   -currentLeader
-//   -#of resistance
-//   -# of spies
-
-
-// Mission
-//   -# of players
-//   -status
-//   -# of fails required
-
-// Round Vote
-
-// Mission Vote
---
+++ js/ui.js
@@ -0,0 +1,64 @@
+window.App = {}
+App.Models = {}
+App.Views = {}
+App.Collections = {}
+App.current = {}
+
+$(document).ready(function(){
+  $(".landing").show()
+
+  $("#start-game").click(function(){
+    $('.landing').hide()
+    $(".num-of-players").show()
+  })
+
+  $("#submit-num-players").click(function(){
+    $(".num-of-players").hide()
+    var numPlayers = parseInt($("#num-players option:selected").val())
+    App.current.game = new App.Models.Game({numPlayers: numPlayers})
+    $(".player-names").show()
+    App.current.players = new App.Collections.Players()
+  })
+
+  $("#player-name").keyup(function(e){
+    if (e.keyCode == 13 && $("#player-name").val().length > 0){
+      var name = $(this).val()
+      var id = App.current.players.length + 1
+      App.current.players.add({name: name, id: id})
+      $("#names-list").append("<li>" + name + "</li>")
+      $(this).val('')
+
+      if (App.current.players.length === App.current.game.numPlayers) {
+        $(this).parent().attr("style", "visibility: hidden;")
+        $("#names-entered").show()
+      }
+    }
+  })
+
+  $("#names-entered").click(function(){
+    $(".player-names").hide()
+    App.current.players.createSpies(App.current.game.numSpies)
+    var players = _.sortBy(App.current.players.models, function(player){
+      player.id
+    })
+
+    App.current.players.displayIdentities()
+
+    $(".pass-to-left").click(function(){
+      $(".find-out .name").text('')
+      $("p.identity").text('')
+      $("p.identity").hide()
+      $(".find-out").hide()
+      App.current.players.displayIdentities()
+    })
+  })
+
+  $(".mission-no").click(function(){
+    App.current.game.continueRound()
+  })
+
+  $(".mission-yes").click(function(){
+
+  })
+
+})
